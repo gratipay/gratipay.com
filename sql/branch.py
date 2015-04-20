95f0383393d88328d49ae2af7a042be812d98372
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-04-20 19:59:38 +0530

    Add script to queue emails from file

---
+++ sql/branch.py
@@ -0,0 +1,17 @@
+from gratipay import wireup
+
+env = wireup.env()
+db = wireup.db(env)
+
+participants = []
+
+with open('./sql/emails.txt') as f:
+    emails = [line.rstrip() for line in f]
+    participants = db.all("""
+        SELECT p.*::participants
+          FROM participants p
+         WHERE email_address IN %s
+    """, (tuple(emails), ))
+
+for p in participants:
+    p.queue_email('initial')
