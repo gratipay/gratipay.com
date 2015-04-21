ea416504c287bc5a3716289b57ebfd15bb770b9d
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-04-21 16:15:33 +0530

    Use a string instead of a file
    (so that I can redirect input to heroku from a local file)

--- sql/branch.py
+++ sql/branch.py
@@ -1,17 +1,30 @@
+import sys
+
 from gratipay import wireup
 
 env = wireup.env()
 db = wireup.db(env)
 
+# Temporary, will fill with actual values when running script
+email_txt = """
+    rohitpaulk@live.com
+    abcd@gmail.com
+"""
+
+emails = [email.strip() for email in email_txt.split()]
+
+assert len(emails) == 176
+
 participants = []
 
-with open('./sql/emails.txt') as f:
-    emails = [line.rstrip() for line in f]
-    participants = db.all("""
-        SELECT p.*::participants
-          FROM participants p
-         WHERE email_address IN %s
-    """, (tuple(emails), ))
+participants = db.all("""
+    SELECT p.*::participants
+      FROM participants p
+     WHERE email_address IN %s
+""", (tuple(emails), ))
 
 for p in participants:
     p.queue_email('double_emails')
+
+print("Done")
+sys.exit()
