77f2c6b87cae807287c3d61c2e5e81e7c71ca7be
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-05-13 21:51:14 +0530

    Basic tests

--- tests/py/test_teams.py
+++ tests/py/test_teams.py
@@ -8,6 +8,15 @@ from gratipay.models.team import Team
 
 class TestNewTeams(Harness):
 
+    valid_data = {
+        'name': 'Gratiteam'
+    }
+
+    def post_new(self, data, auth_as='alice', expected=200):
+        r =  self.client.POST('/new', data=data, auth_as=auth_as, raise_immediately=False)
+        assert r.code == expected
+        return r
+
     def test_harness_can_make_a_team(self):
         team = self.make_team()
         assert team.name == 'The A Team'
@@ -23,6 +32,17 @@ class TestNewTeams(Harness):
         team = Team.from_id(self.make_team().id)
         assert team.name == 'The A Team'
         assert team.owner == 'hannibal'
+
+    def test_can_create_new_team(self):
+        self.make_participant('alice')
+        self.post_new(self.valid_data)
+        team = self.db.one("SELECT * FROM teams")
+        assert team
+        assert team.owner == 'alice'
+
+    def test_401_for_anon_creating_new_team(self):
+        self.post_new(self.valid_data, auth_as=None, expected=401)
+        assert self.db.one("SELECT COUNT(*) FROM teams") == 0
 
 
 class TestOldTeams(Harness):
