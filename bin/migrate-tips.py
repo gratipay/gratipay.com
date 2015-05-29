c12d70090b47765a658a98c29fd332ca6ec057d7
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-05-29 07:02:58 +0530

    Add script for migrating tips to new teams

---
+++ bin/migrate-tips.py
@@ -0,0 +1,20 @@
+from gratipay.wireup import db, env
+from gratipay.models.team import Team, AlreadyMigrated
+
+db = db(env())
+
+slugs = db.all("""
+    SELECT slug
+      FROM teams
+     WHERE is_approved IS TRUE
+""")
+
+for slug in slugs:
+    team = Team.from_slug(slug)
+    try:
+        team.migrate_tips()
+        print("Migrated tips for '%s'" % slug)
+    except AlreadyMigrated:
+        print("'%s' already migrated." % slug)
+
+print("Done.")
