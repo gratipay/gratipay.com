gratipay/gratipay.com

commit d58af0b73b61fc23aa3730489bf34c4209d06b88

Clean and DRY the SQL behind payout and masspay

bin/masspay.py
@@ -29,6 +29,7 @@ from decimal import Decimal as D, ROUND_HALF_UP
 
 import requests
 from gratipay import wireup
+from gratipay.billing.exchanges import get_ready_payout_routes_by_network
 from httplib import IncompleteRead
 
 
@@ -121,50 +122,29 @@ class Payee(object):
 
 def compute_input_csv():
     db = wireup.db(wireup.env())
-    participants = db.all("""
-
-        SELECT p.*, r.address AS paypal_email, r.fee_cap AS paypal_fee_cap
-          FROM exchange_routes r
-          JOIN participants p ON p.id = r.participant
-         WHERE r.network = 'paypal'
-           AND p.balance > 0
-
-          ---- Only include team owners
-          ---- TODO: Include members on payroll once process_payroll is implemented
-
-           AND (( SELECT count(*)
-                   FROM teams t
-                  WHERE t.owner = p.username
-                    AND t.is_approved IS TRUE
-                    AND t.is_closed IS NOT TRUE
-                ) > 0
-               OR p.status_of_1_0_balance='pending-payout')
-
-      ORDER BY p.balance DESC
-
-    """)
+    routes = get_ready_payout_routes_by_network(db, 'paypal')
     writer = csv.writer(open(INPUT_CSV, 'w+'))
     print_rule(88)
     headers = "username", "email", "fee cap", "balance", "tips", "amount"
     print("{:<24}{:<32} {:^7} {:^7} {:^7} {:^7}".format(*headers))
     print_rule(88)
     total_gross = 0
-    for participant in participants:
-        total = participant.giving
-        amount = participant.balance - total
+    for route in routes:
+        total = route.participant.giving
+        amount = route.participant.balance - total
         if amount < 0.50:
             # Minimum payout of 50 cents. I think that otherwise PayPal upcharges to a penny.
             # See https://github.com/gratipay/gratipay.com/issues/1958.
             continue
         total_gross += amount
-        print("{:<24}{:<32} {:>7} {:>7} {:>7} {:>7}".format( participant.username
-                                                           , participant.paypal_email
-                                                           , participant.paypal_fee_cap
-                                                           , participant.balance
+        print("{:<24}{:<32} {:>7} {:>7} {:>7} {:>7}".format( route.participant.username
+                                                           , route.address
+                                                           , route.fee_cap
+                                                           , route.participant.balance
                                                            , total
                                                            , amount
                                                             ))
-        row = (participant.username, participant.paypal_email, participant.paypal_fee_cap, amount)
+        row = (route.username, route.address, route.fee_cap, amount)
         writer.writerow(row)
     print(" "*80, "-"*7)
     print("{:>88}".format(total_gross))

gratipay/billing/exchanges.py
@@ -293,6 +293,36 @@ def _prep_hit(unrounded):
     return cents, amount_str, upcharged, fee
 
 
+def get_ready_payout_routes_by_network(db, network):
+    hack = db.all("""
+        SELECT p.*::participants, r.*::exchange_routes
+          FROM participants p
+          JOIN current_exchange_routes r ON p.id = r.participant
+         WHERE p.balance > 0
+           AND r.network = %s
+
+      ---- Only include team owners
+      ---- TODO: Include members on payroll once process_payroll is implemented
+
+           AND (( SELECT count(*)
+                   FROM teams t
+                  WHERE t.owner = p.username
+                    AND t.is_approved IS TRUE
+                    AND t.is_closed IS NOT TRUE
+                ) > 0
+               OR p.status_of_1_0_balance='pending-payout')
+
+    """, (network,))
+
+    # Work around lack of proper nesting in postgres.orm.
+    out = []
+    for participant, route in hack:
+        route.__dict__['participant'] = participant
+        out.append(route)
+
+    return out
+
+
 def record_exchange(db, route, amount, fee, participant, status, error=None):
     """Given a Bunch of Stuff, return an int (exchange_id).
 

gratipay/billing/payday.py
@@ -19,7 +19,8 @@ import braintree
 import aspen.utils
 from aspen import log
 from gratipay.billing.exchanges import (
-    ach_credit, cancel_card_hold, capture_card_hold, create_card_hold, upcharge
+    ach_credit, cancel_card_hold, capture_card_hold, create_card_hold, upcharge,
+    get_ready_payout_routes_by_network
 )
 from gratipay.exceptions import NegativeBalance
 from gratipay.models import check_db
@@ -406,35 +407,17 @@ class Payday(object):
         bank accounts of participants.
         """
         log("Starting payout loop.")
-        participants = self.db.all("""
-            SELECT p.*::participants
-              FROM exchange_routes r
-              JOIN participants p ON p.id = r.participant
-             WHERE r.network = 'balanced-ba'
-               AND p.balance > 0
-
-          ---- Only include team owners
-          ---- TODO: Include members on payroll once process_payroll is implemented
-
-               AND (( SELECT count(*)
-                       FROM teams t
-                      WHERE t.owner = p.username
-                        AND t.is_approved IS TRUE
-                        AND t.is_closed IS NOT TRUE
-                    ) > 0
-                   OR p.status_of_1_0_balance='pending-payout')
-
-        """)
-        def credit(participant):
-            if participant.is_suspicious is None:
-                log("UNREVIEWED: %s" % participant.username)
+        routes = get_ready_payout_routes_by_network(self.db, 'balanced-ba')
+        def credit(route):
+            if route.participant.is_suspicious is None:
+                log("UNREVIEWED: %s" % route.participant.username)
                 return
-            withhold = participant.giving
-            error = ach_credit(self.db, participant, withhold)
+            withhold = route.participant.giving
+            error = ach_credit(self.db, route.participant, withhold)
             if error:
                 self.mark_ach_failed()
-        threaded_map(credit, participants)
-        log("Did payout for %d participants." % len(participants))
+        threaded_map(credit, routes)
+        log("Did payout for %d participants." % len(routes))
         self.db.self_check()
         log("Checked the DB.")
 

commit db90b2811976244c16b4e7ce810d3aa0d41487c1

Fix a MassPay regression
Introduced in d58af0b73b61fc23aa3730489bf34c4209d06b88

bin/masspay.py
@@ -144,7 +144,7 @@ def compute_input_csv():
                                                            , total
                                                            , amount
                                                             ))
-        row = (route.username, route.address, route.fee_cap, amount)
+        row = (route.participant.username, route.address, route.fee_cap, amount)
         writer.writerow(row)
     print(" "*80, "-"*7)
     print("{:>88}".format(total_gross))