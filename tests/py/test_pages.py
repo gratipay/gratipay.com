7ad9bf7dba7879c4b46469324be9b6dc5ab08444
Chad Whitacre <chad@zetaweb.com>
2015-06-17 17:32:00 -0400

    Avoid hitting the Braintree API in this test

--- tests/py/test_pages.py
+++ tests/py/test_pages.py
@@ -5,6 +5,7 @@ import re
 from aspen import Response
 
 import pytest
+import mock
 from gratipay.security.user import SESSION
 from gratipay.testing import Harness
 from gratipay.wireup import find_files
@@ -208,7 +209,9 @@ class TestPages(Harness):
         headers = self.client.GET('/about/').headers
         assert headers['X-XSS-Protection'] == '1; mode=block'
 
-    def test_balanced_removed_from_credit_card_page(self):
+    @mock.patch('gratipay.models.participant.Participant.get_braintree_account')
+    @mock.patch('gratipay.models.participant.Participant.get_braintree_token')
+    def test_balanced_removed_from_credit_card_page(self, foo, bar):
         self.make_participant('alice', claimed_time='now')
         body = self.client.GET("/~alice/routes/credit-card.html", auth_as="alice").body
         assert  "Balanced Payments" not in body
