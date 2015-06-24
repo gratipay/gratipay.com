74a3ea735f775650dbbd97baa50321a674d4eb2e
rorepo <bj_knr@yahoo.com>
2015-06-24 12:09:38 +0100

    fake-data, teams and subscriptions

--- tests/py/test_fake_data.py
+++ tests/py/test_fake_data.py
@@ -18,6 +18,13 @@ class TestFakeData(Harness):
         tips = self.db.all("SELECT * FROM tips")
         participants = self.db.all("SELECT * FROM participants")
         transfers = self.db.all("SELECT * FROM transfers")
+        teams = self.db.all("SELECT * FROM teams")
+        subscriptions = self.db.all("SELECT * FROM subscriptions")
         assert len(tips) == num_tips
-        assert len(participants) == num_participants + num_teams
+        assert len(participants) == num_participants
         assert len(transfers) == num_transfers
+        assert len(teams) == num_teams
+        if num_tips <= num_participants - num_teams:
+            assert len(subscriptions) == num_tips
+        else:
+            assert len(subscriptions) == (num_participants - num_teams) 
