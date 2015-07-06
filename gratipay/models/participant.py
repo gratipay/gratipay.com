91cae6da7b0cd773991d38736dd6263fc757269e
Chad Whitacre <chad@zetaweb.com>
2015-07-06 16:25:00 -0400

    Don't consider balanced valid for has_payout_route

--- gratipay/models/participant.py
+++ gratipay/models/participant.py
@@ -773,7 +773,7 @@ class Participant(Model, MixinTeam):
 
     @property
     def has_payout_route(self):
-        for network in ('balanced-ba', 'paypal'):
+        for network in ('paypal',):
             route = ExchangeRoute.from_network(self, network)
             if route and not route.error:
                 return True
