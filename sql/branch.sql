43ef5a2786de7054b4f0e3edb1ad03090ed10341
Chad Whitacre <chad@zetaweb.com>
2015-08-20 11:11:05 -0400

    Preen
     - Flip order of Onboarding and To-do
     - Standardize capitalization of Self-onboarding and To-do

--- sql/branch.sql
+++ sql/branch.sql
@@ -4,12 +4,12 @@ BEGIN;
     ALTER TABLE teams DROP COLUMN getting_involved;
     ALTER TABLE teams DROP COLUMN getting_paid;
 
-    ALTER TABLE teams ADD COLUMN todo_url text NOT NULL DEFAULT '';
     ALTER TABLE teams ADD COLUMN onboarding_url text NOT NULL DEFAULT '';
+    ALTER TABLE teams ADD COLUMN todo_url text NOT NULL DEFAULT '';
 
     UPDATE teams
-       SET todo_url='https://github.com/gratipay'
-         , onboarding_url='http://inside.gratipay.com/big-picture/welcome'
+       SET onboarding_url='http://inside.gratipay.com/big-picture/welcome'
+         , todo_url='https://github.com/gratipay'
      WHERE slug='Gratipay';
 
 END;
--- tests/py/test_teams.py
+++ tests/py/test_teams.py
@@ -12,8 +12,8 @@ class TestTeams(Harness):
         'name': 'Gratiteam',
         'product_or_service': 'We make widgets.',
         'homepage': 'http://gratipay.com/',
-        'todo_url': 'https://github.com/gratipay',
         'onboarding_url': 'http://inside.gratipay.com/',
+        'todo_url': 'https://github.com/gratipay',
         'agree_public': 'true',
         'agree_payroll': 'true',
         'agree_terms': 'true',
@@ -54,8 +54,8 @@ class TestTeams(Harness):
         assert team.name == 'Gratiteam'
         assert team.homepage == 'http://gratipay.com/'
         assert team.product_or_service == 'We make widgets.'
-        assert team.todo_url == 'https://github.com/gratipay'
         assert team.onboarding_url == 'http://inside.gratipay.com/'
+        assert team.todo_url == 'https://github.com/gratipay'
 
     def test_401_for_anon_creating_new_team(self):
         self.post_new(self.valid_data, auth_as=None, expected=401)
@@ -112,11 +112,11 @@ class TestTeams(Harness):
         assert self.db.one("SELECT COUNT(*) FROM teams") == 0
         assert "Please enter an http[s]:// URL for the 'Homepage' field." in r.body
 
-        r = self.post_new(dict(self.valid_data, todo_url='foo'), expected=400)
-        assert "Please enter an http[s]:// URL for the 'Issue Tracker URL' field." in r.body
-
         r = self.post_new(dict(self.valid_data, onboarding_url='foo'), expected=400)
-        assert "Please enter an http[s]:// URL for the 'Self-Onboarding Documentation URL' field." in r.body
+        assert "an http[s]:// URL for the 'Self-onboarding Documentation URL' field." in r.body
+
+        r = self.post_new(dict(self.valid_data, todo_url='foo'), expected=400)
+        assert "Please enter an http[s]:// URL for the 'To-do URL' field." in r.body
 
     def test_error_message_for_slug_collision(self):
         self.make_participant('alice', claimed_time='now', email_address='alice@example.com', last_paypal_result='')
