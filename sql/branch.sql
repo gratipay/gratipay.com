1951c23bc8e3a64c0ca481edbcec9e61162147f9
Chad Whitacre <chad@zetaweb.com>
2015-08-20 10:49:41 -0400

    Backfill links for the Gratipay Gratipay Team

--- sql/branch.sql
+++ sql/branch.sql
@@ -7,4 +7,9 @@ BEGIN;
     ALTER TABLE teams ADD COLUMN todo_url text NOT NULL DEFAULT '';
     ALTER TABLE teams ADD COLUMN onboarding_url text NOT NULL DEFAULT '';
 
+    UPDATE teams
+       SET todo_url='https://github.com/gratipay'
+         , onboarding_url='http://inside.gratipay.com/big-picture/welcome'
+     WHERE slug='Gratipay';
+
 END;
