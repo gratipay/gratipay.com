457bd9d977af147137319318b7b5e7d6a8fa26df
Chad Whitacre <chad@zetaweb.com>
2015-08-20 23:29:18 -0400

    Leave data for Team owners to salvage

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,9 +1,5 @@
 BEGIN;
 
-    ALTER TABLE teams DROP COLUMN revenue_model;
-    ALTER TABLE teams DROP COLUMN getting_involved;
-    ALTER TABLE teams DROP COLUMN getting_paid;
-
     ALTER TABLE teams ADD COLUMN onboarding_url text NOT NULL DEFAULT '';
     ALTER TABLE teams ADD COLUMN todo_url text NOT NULL DEFAULT '';
 
