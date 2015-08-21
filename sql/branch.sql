29b8c1d30119001e2282cc00f662c828d889f073
Chad Whitacre <chad@zetaweb.com>
2015-08-20 23:41:42 -0400

    Drop NOT NULL constraints from vestigial fields

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,5 +1,9 @@
 BEGIN;
 
+    ALTER TABLE teams ALTER COLUMN revenue_model DROP NOT NULL;
+    ALTER TABLE teams ALTER COLUMN getting_involved DROP NOT NULL;
+    ALTER TABLE teams ALTER COLUMN getting_paid DROP NOT NULL;
+
     ALTER TABLE teams ADD COLUMN onboarding_url text NOT NULL DEFAULT '';
     ALTER TABLE teams ADD COLUMN todo_url text NOT NULL DEFAULT '';
 
