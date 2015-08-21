4e466e6e394c98d56538452a14e26459ca47e9fb
Chad Whitacre <chad@zetaweb.com>
2015-08-21 18:11:37 -0400

    More Enterprise updates in tests
    These tests appear to be obsolete, but ...

--- tests/py/test_members_json.py
+++ tests/py/test_members_json.py
@@ -10,31 +10,31 @@ from aspen import Response
 class Tests(Harness):
 
     def hit_members_json(self, method='GET', auth_as=None):
-        response = self.client.GET('/~A-Team/members/index.json', auth_as=auth_as)
+        response = self.client.GET('/~Enterprise/members/index.json', auth_as=auth_as)
         return json.loads(response.body)
 
 
     @pytest.mark.xfail(reason='migrating to Teams; see #3399')
     def test_team_has_members(self):
-        team = self.make_participant('A-Team', number='plural', claimed_time='now')
+        team = self.make_participant('Enterprise', number='plural', claimed_time='now')
         team.add_member(self.make_participant('alice', claimed_time='now'))
         team.add_member(self.make_participant('bob', claimed_time='now'))
         team.add_member(self.make_participant('carl', claimed_time='now'))
 
         actual = [x['username'] for x in self.hit_members_json()]
-        assert actual == ['carl', 'bob', 'alice', 'A-Team']
+        assert actual == ['carl', 'bob', 'alice', 'Enterprise']
 
     @pytest.mark.xfail(reason='migrating to Teams; see #3399')
     def test_team_admin_can_get_bare_bones_list(self):
-        self.make_participant('A-Team', number='plural', claimed_time='now')
-        actual = [x['username'] for x in self.hit_members_json(auth_as='A-Team')]
-        assert actual == ['A-Team']
+        self.make_participant('Enterprise', number='plural', claimed_time='now')
+        actual = [x['username'] for x in self.hit_members_json(auth_as='Enterprise')]
+        assert actual == ['Enterprise']
 
     def test_anon_cant_get_bare_bones_list(self):
-        self.make_participant('A-Team', number='plural', claimed_time='now')
+        self.make_participant('Enterprise', number='plural', claimed_time='now')
         assert pytest.raises(Response, self.hit_members_json).value.code == 404
 
     def test_non_admin_cant_get_bare_bones_list(self):
-        self.make_participant('A-Team', number='plural', claimed_time='now')
+        self.make_participant('Enterprise', number='plural', claimed_time='now')
         self.make_participant('alice', claimed_time='now')
         assert pytest.raises(Response, self.hit_members_json, auth_as='alice').value.code == 404
