cd45031d443fd759d3f7e8f986900c5da7c9b614
Chad Whitacre <chad@zetaweb.com>
2015-08-28 05:03:20 -0400

    I think that's it? Responsiveness!

--- js/gratipay/jump.js
+++ js/gratipay/jump.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    $('#jump').submit(function (e) {
+    $('.jump-box').submit(function (e) {
         e.preventDefault();
         e.stopPropagation();
 
-        var platform = Gratipay.trim($('#jump select').val()),
-            val      = Gratipay.trim($('#jump input').val());
+        var platform = Gratipay.trim($('.jump-box select').val()),
+            val      = Gratipay.trim($('.jump-box input').val());
         if (val) window.location = '/on/' + platform + '/' + val + '/';
     });
 });
