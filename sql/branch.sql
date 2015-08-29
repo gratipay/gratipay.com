fd534ed2960ba23615cd0a12360327e72b65ea92
Chad Whitacre <chad@zetaweb.com>
2015-08-29 17:55:55 -0400

    Now for `receiving` ...

--- sql/branch.sql
+++ sql/branch.sql
@@ -2,5 +2,6 @@ BEGIN;
 
     ALTER TABLE participants DROP COLUMN anonymous_receiving;
     ALTER TABLE participants DROP COLUMN npatrons;
+    ALTER TABLE participants DROP COLUMN receiving;
 
 END;
