69acd79f8d09e25986caa531850476d58f0e77df
Chad Whitacre <chad@zetaweb.com>
2015-08-31 12:13:52 -0400

    Make the volume stats accurate
    They were doubled because we were counting both sides, payment and
    payroll. We should just count payment.

--- sql/update-payday-numbers.sql
+++ sql/update-payday-numbers.sql
@@ -3,5 +3,7 @@ UPDATE paydays p
    SET nactive = (SELECT DISTINCT count(*) FROM (
         SELECT participant FROM payments WHERE payday=p.id
        ) AS foo)
-     , volume = (SELECT COALESCE(sum(amount), 0) FROM payments WHERE payday=p.id)
+     , volume = (SELECT COALESCE(sum(amount), 0)
+                   FROM payments
+                  WHERE payday=p.id AND direction='to-team')
  WHERE ts_start > '2015-05-07'::timestamptz;
