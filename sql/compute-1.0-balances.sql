dc74fb19b011ba2a99490268b0112b1d89533422
Chad Whitacre <chad@zetaweb.com>
2015-09-03 23:06:41 -0400

    We want these SQL scripts after all
    These scripts help us mark as `completed` those participants who
    received before the Gratipaypocalypse, but withdrew or gave away any
    money they had left at the Gratipocalypse.
    
    But ... we don't need the balance_1_0 column, and we do need to handle
    an edge case with exchanges.

--- sql/compute-1.0-balances.sql
+++ sql/compute-1.0-balances.sql
@@ -2,3 +2,12 @@ UPDATE numbers_1_0 SET at_gratipocalypse =
     (balance - paid_in + paid_out - transferred_to + transferred_from - paid_to + paid_from);
 UPDATE numbers_1_0 SET remaining =
     (at_gratipocalypse - paid_out - transferred_from - paid_from);
+
+-- Manually correct two users per https://github.com/gratipay/gratipay.com/issues/3453
+-- The other two affected users there currently have a zero balance, so they're
+-- not included in receiving_1_0 (any Gratipay 1.0 money they accumulated is
+-- already gone).
+
+UPDATE numbers_1_0
+   SET paid_in = paid_in - paid_out, paid_out = 0
+ WHERE participant IN ('webmaven', 'whit537');
