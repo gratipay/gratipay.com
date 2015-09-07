4d8d5d0b15e0867dd5f5aa4b6214438c6882ccb3
Chad Whitacre <chad@zetaweb.com>
2015-09-07 11:34:50 -0400

    Decouple status update from deployment
    Setting the status_of_1_0_payout field depends on the numbers_1_0 table,
    which is expensive to create. Let's keep the schema changes in
    branch.sql but move the status update to a separate script that we can
    run once deployment is finished. This'll require a second PR to drop the
    status_of_1_0_balance column/function/trigger once we're done.

--- sql/1.0/all.sql
+++ sql/1.0/all.sql
@@ -1,3 +1,4 @@
 \i sql/1.0/identify-receivers.sql
 \i sql/1.0/prep-numbers.sql
 \i sql/1.0/compute-balances.sql
+\i sql/1.0/set-status.sql
--- sql/branch.sql
+++ sql/branch.sql
@@ -1,26 +1,8 @@
 BEGIN;
-    \i sql/1.0/all.sql
-
     CREATE TYPE status_of_1_0_payout AS ENUM
         ('pending-application', 'pending-review', 'rejected', 'pending-payout', 'completed');
     ALTER TABLE participants ADD COLUMN status_of_1_0_payout status_of_1_0_payout
         NOT NULL DEFAULT 'completed';
-
-    UPDATE participants
-       SET status_of_1_0_payout = 'pending-application'
-     WHERE username = ANY(SELECT participant FROM numbers_1_0 WHERE remaining > 0);
-
-    UPDATE participants
-       SET status_of_1_0_payout='pending-payout'
-     WHERE status_of_1_0_balance='pending-payout';
-
-    UPDATE participants
-       SET status_of_1_0_payout='completed'
-     WHERE status_of_1_0_balance='resolved';
-
-    ALTER TABLE participants DROP COLUMN status_of_1_0_balance;
-    DROP TRIGGER update_status_of_1_0_balance ON participants;
-    DROP FUNCTION set_status_of_1_0_balance_to_resolved();
 
     CREATE FUNCTION complete_1_0_payout() RETURNS trigger AS $$
         BEGIN
