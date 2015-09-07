bad02139a33e6243e090a5a47b91893a9f00647a
Chad Whitacre <chad@zetaweb.com>
2015-09-07 13:49:57 -0400

    Wire up an admin knob for payout status
    This replaces the admin knob for *balance* status.

--- tests/py/test_payout_status.py
+++ tests/py/test_payout_status.py
@@ -3,7 +3,8 @@ from __future__ import print_function, unicode_literals
 from gratipay.testing import Harness
 from gratipay.models.participant import Participant
 
-workflow = ['pending-application', 'pending-review', 'rejected', 'pending-payout', 'completed']
+workflow = ['too-little', 'pending-application', 'pending-review', 'rejected', 'pending-payout',
+            'completed']
 
 
 class Tests(Harness):
@@ -12,22 +13,19 @@ class Tests(Harness):
         self.make_participant('alice', claimed_time='now')
         self.make_participant('admin', claimed_time='now', is_admin=True)
 
-    def hit(self, new_status, auth_as='admin'):
-        return self.client.PxST(
-            "/~alice/payout-status",
-            {'to': new_status},
-            auth_as=auth_as
-        )
+    def hit(self, new_status, expecting_error=True, auth_as='admin'):
+        method = self.client.PxST if expecting_error else self.client.POST
+        return method("/~alice/payout-status", {'to': new_status}, auth_as=auth_as)
 
     def test_admin_can_change_status(self):
         for status in workflow:
-            response = self.hit(status)
-            assert response.code == 302
+            response = self.hit(status, expecting_error=False)
+            assert response.code == 200
             assert Participant.from_username('alice').status_of_1_0_payout == status
 
     def test_user_cant_change_status(self):
         response = self.hit('pending-payout', auth_as='alice')
-        assert response.code == 401
+        assert response.code == 403
         assert Participant.from_username('alice').status_of_1_0_payout == 'completed'
 
     def test_invalid_is_400(self):
