b2fd2ad4b37e845236935a773f6a92f8cb7e7bb1
Chad Whitacre <chad@zetaweb.com>
2015-09-14 15:49:13 -0400

    merge branch.sql into schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -685,3 +685,20 @@ BEGIN;
     ALTER TABLE teams ADD COLUMN image_oid_small oid NOT NULL DEFAULT 0;
     ALTER TABLE teams ADD COLUMN image_type supported_image_types;
 END;
+
+
+-- https://github.com/gratipay/gratipay.com/pull/3785
+BEGIN;
+
+    CREATE FUNCTION current_payday() RETURNS paydays AS $$
+        SELECT *
+          FROM paydays
+         WHERE ts_end='1970-01-01T00:00:00+00'::timestamptz;
+    $$ LANGUAGE sql;
+
+    CREATE FUNCTION current_payday_id() RETURNS int AS $$
+        -- This is a function so we can use it in DEFAULTS for a column.
+        SELECT id FROM current_payday();
+    $$ LANGUAGE sql;
+
+END;
