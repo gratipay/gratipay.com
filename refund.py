137aea3b9a4b35a359c77da6b55e81fd51b52365
Chad Whitacre <chad@zetaweb.com>
2015-10-08 21:55:11 -0400

    Emit a log line for each user with a balance

--- refund.py
+++ refund.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -u
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from gratipay import wireup
@@ -8,11 +8,13 @@ db = wireup.db(wireup.env())
 
 
 def get_participants(cur):
-    return cur.all("SELECT participants.*::participants FROM participants WHERE balance > 0 LIMIT 10")
+    return cur.all("SELECT participants.*::participants FROM participants WHERE balance > 0")
 
 
-def get_refund(cur, participant):
-    exchanges = cur.all("SELECT * FROM exchanges WHERE participant=%s", (participant.username,))
+def process_participant(cur, participant):
+    exchanges = cur.all( "SELECT * FROM exchanges JOIN exchange_routes WHERE participant=%s"
+                       , (participant.username,)
+                        )
     payments = cur.all("SELECT * FROM payments WHERE participant=%s", (participant.username,))
     transfers = cur.all( "SELECT * FROM transfers "
                          "WHERE (tipper=%(username)s OR tippee=%(username)s)"
@@ -20,47 +22,47 @@ def get_refund(cur, participant):
                         )
     get_timestamp = lambda t: t['timestamp']
     transactions = sorted(exchanges+payments+transfers, key=get_timestamp, reverse=True)
-    balance = participant.balance
+    amount = balance = participant.balance
+    exchange = None
     for t in transactions:
+        if balance <= 0: break
         if 'fee' in t:
             if t['status'] == 'failed':
                 continue
             if t['amount'] > 0:
-                kind = 'cc'
                 assert t['status'] in (None, 'succeeded')
-                balance -= t['amount']
+                exchange = t
+                break
             else:
-                kind = 'ba'
                 balance -= t['amount'] - t['fee']
         elif 'direction' in t:
             if t['direction'] == 'to-participant':
-                kind = 'take'
                 balance -= t['amount']
             else:
-                kind = 'payment'
                 assert t['direction'] == 'to-team'
                 balance += t['amount']
         else:
-            t['kind'] = 'transfer'
             if t['tippee'] == participant.username:
-                kind = 'tip-received'
                 balance -= t['amount']
             else:
-                kind = 'tip-given'
                 balance += t['amount']
+        #print('{timestamp:<32} {kind:<12} {balance:>5}'.format(**t))
 
-        yield {'balance': _balance, 'kind': kind, 'timestamp': str(t['timestamp'])}
-        if balance <= 0 or kind == 'deposit':
-            break
-
-
-def process_participant(cur, participant):
-    print()
-    print(participant.username)
-    print('='*70)
-
-    for t in get_transactions(cur, participant):
-        print('{timestamp:<32} {kind:<12} {balance:>5}'.format(**t))
+    params = dict( timestamp = t['timestamp']
+                 , refund_amount = amount
+                 , username = participant.username
+                 , exchange_id = ''
+                 , exchange_amount = ''
+                 , yesno = ' no'
+                  )
+    if exchange:
+        params['exchange_id'] = exchange['id']
+        params['exchange_amount'] = exchange['amount']
+        params['yesno'] = 'yes'
+        if amount > exchange['amount']:
+            params['yesno'] = 'BAD'
+    print('{timestamp} {yesno} {exchange_id:>6} {refund_amount:>7} {exchange_amount:>7} {username}'
+          .format(**params))
 
 
 with db.get_cursor(back_as=dict) as cur:
@@ -68,5 +70,4 @@ with db.get_cursor(back_as=dict) as cur:
         for participant in get_participants(cur):
             process_participant(cur, participant)
     finally:
-        print('Rolling back ...')
         cur.connection.rollback()
---
+++ tally-refunds.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import commands
+
+
+def title(title):
+    print(title)
+    print("-"*34)
+
+def report(*patterns):
+    N = 0
+    for pattern in patterns:
+        n = int(commands.getoutput('grep "{}" refund.log | wc -l'.format(pattern)))
+        N += n
+        print("{:<28} {:>5}".format(pattern, n))
+    print("{:<28} {:>5}".format('', N))
+
+
+title("Hi-level")
+report(" yes ", " no ", " BAD ")
+
+title("Check")
+report("^201\d-")
