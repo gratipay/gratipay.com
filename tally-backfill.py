b04ac4f0f059bdaac6908562ca0c1e036704cb3d
Chad Whitacre <chad@zetaweb.com>
2015-10-09 23:56:48 -0400

    Revert "Remove scripts that were added in #3807."
    This reverts commit 90b4a4b24cd0cfba7f7d966245be554d76cf4c18.

---
+++ tally-backfill.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import commands
+
+
+def title(title):
+    print(title)
+    print("-"*34)
+
+def report(*patterns):
+    N = 0
+    for pattern in patterns:
+        n = int(commands.getoutput('grep "{}" backfill.log | wc -l'.format(pattern)))
+        N += n
+        print("{:<28} {:>5}".format(pattern, n))
+    print("{:<28} {:>5}".format('', N))
+
+
+title("Hi-level")
+report("Linking", "Already linked", "raise")
+
+title("Participants")
+report("known customer", "raise UnknownCustomer")
+
+title("Exchanges")
+report("exchange_id in transaction", "triangulated an exchange", "raise UnknownExchange")
+
+title("Routes")
+report("exchange has a route", "card matches", "customer matches", "created a route",
+       "raise AmbiguousRoute", "raise AmbiguousCustomer", "raise UnknownRoute")
+
+title("Check")
+report("^201\d-")
