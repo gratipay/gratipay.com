3c4a9c08858378624600a3f64616f03e29d21f31
Chad Whitacre <chad@zetaweb.com>
2015-10-10 00:29:28 -0400

    Use threaded_map to speed up refunds

--- actually-do-refunds.py
+++ actually-do-refunds.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python -u
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from gratipay.billing.payday import threaded_map
 import csv, os, requests
+import threading
 
 url = 'https://api.balancedpayments.com/debits/{}/refunds'
 balanced_api_secret = os.environ['BALANCED_API_SECRET']
 
 inp = csv.reader(open('refunds.csv'))
 out = csv.writer(open('refunds.completed.csv', 'w+'))
+writelock = threading.Lock()
 
-for ts, id, amount, username, route_id in inp:
+def refund(row):
+    ts, id, amount, username, route_id = row
     response = requests.post( url.format(id)
                             , data={'amount': amount}
                             , auth=(balanced_api_secret, '')
                              )
-    out.writerow((ts,id,amount,username,route_id,response.status_code,response.content))
+
+    writelock.acquire()
+    try:
+        out.writerow((ts,id,amount,username,route_id,response.status_code,response.content))
+    finally:
+        writelock.release()
+    return
+
+threaded_map(refund, inp)
