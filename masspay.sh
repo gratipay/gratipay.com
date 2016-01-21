a2ec66f2e50d763b079db1433c6424ffcd228804
Paul Kuruvilla <rohitpaulk@live.com>
2016-01-21 23:25:57 +0530

    Specify heroku app in masspay.sh

--- masspay.sh
+++ masspay.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-heroku config -s | honcho run -e /dev/stdin ./env/bin/python ./bin/masspay.py -i
-./bin/masspay.py -o
+heroku config -s -a gratipay | ./env/bin/honcho run -e /dev/stdin ./env/bin/python ./bin/masspay.py -i
+./env/bin/python ./bin/masspay.py -o
