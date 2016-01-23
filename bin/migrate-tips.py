9443faca028f05d74713450a8c15d1053589b4a5
aandis <abhishekrm@iitrpr.ac.in>
2016-01-24 00:58:42 +0530

    Optimized team fetching query.

--- bin/migrate-tips.py
+++ bin/migrate-tips.py
@@ -1,20 +1,26 @@
 from gratipay.wireup import db, env
-from gratipay.models.team import Team, AlreadyMigrated
+from gratipay.models.team import AlreadyMigrated
 
 db = db(env())
 
-slugs = db.all("""
-    SELECT slug
-      FROM teams
-     WHERE is_approved IS TRUE
+teams = db.all("""
+    SELECT distinct ON (t.slug) t.*::teams
+      FROM teams t
+      JOIN tips ON t.owner = tips.tippee    -- Only fetch teams whose owner have tips.
+     WHERE t.is_approved IS TRUE            -- Only fetch approved teams.
+       AND NOT EXISTS (                     -- Make sure not already migrated.
+            SELECT 1
+              FROM payment_instructions pi
+             WHERE t.slug = pi.team
+               AND pi.ctime < t.ctime
+	     )
 """)
 
-for slug in slugs:
-    team = Team.from_slug(slug)
+for team in teams:
     try:
         ntips = team.migrate_tips()
-        print("Migrated {} tip(s) for '{}'".format(ntips, slug))
+        print("Migrated {} tip(s) for '{}'".format(ntips, team.slug))
     except AlreadyMigrated:
-        print("'%s' already migrated." % slug)
+        print("'%s' already migrated." % team.slug)
 
 print("Done.")
