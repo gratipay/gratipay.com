b2dd1f94a7e0913e94e0a56b0ab842e58742ccbf
aandis <abhishekrm@iitrpr.ac.in>
2016-02-27 13:45:23 +0530

    Sent all fields/values instead of only updated ones.

--- js/gratipay/edit_team.js
+++ js/gratipay/edit_team.js
@@ -2,50 +2,29 @@ Gratipay.edit_team = {}
 
 Gratipay.edit_team.initForm = function() {
     $form = $("#edit-team");
-    $buttons = $form.find("button");
-
-    $form.find(":input").each(function() {
-        // Save original values.
-        $(this).data('original', $(this).val());
-    });
+    $buttons = $form.find("button"); // submit and cancel btns
     $form.submit(Gratipay.edit_team.submitForm);
 }
 
 Gratipay.edit_team.submitForm = function(e) {
     e.preventDefault();
 
-    var modified = false;
+    var data = new FormData($form[0]);
 
-    var data = new FormData();
-    $form.find(":input").each(function() {
-        if($(this).data("original") !== $(this).val()) {
-            modified = true;
-
-            var name = $(this).attr("name");
-            if (name === "image") {
-                data.append(name, $(this)[0].files[0]);
-            } else {
-                data.append(name, $(this).val());
-            }
-        }
+    $buttons.prop("disabled", true);
+    $.ajax({
+        url: $form.attr("action"),
+        type: $form.attr("method"),
+        data: data,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        success: function(d) {
+            Gratipay.notification("Successfully edited team.", 'success');
+            setTimeout(function() {
+                window.location.href = "../";
+            }, 1000);
+        },
+        error: [Gratipay.error, function () { $buttons.prop("disabled", false); }]
     });
-
-    if(modified) {
-        $buttons.prop("disabled", true);
-        $.ajax({
-            url: $form.attr("action"),
-            type: $form.attr("method"),
-            data: data,
-            dataType: 'json',
-            processData: false,
-            contentType: false,
-            success: function(d) {
-                Gratipay.notification("Successfully edited team.", 'success');
-                setTimeout(function() {
-                    window.location.href = "../";
-                }, 1000);
-            },
-            error: [Gratipay.error, function () { $buttons.prop("disabled", false); }]
-        });
-    }
 }
