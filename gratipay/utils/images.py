b1d1f5b3786d1a1a257a4a6b2527abb7172ab3d7
aandis <abhishekrm@iitrpr.ac.in>
2016-03-03 21:31:30 +0530

    Improved imgize structure.

--- gratipay/utils/images.py
+++ gratipay/utils/images.py
@@ -8,11 +8,22 @@ def imgize(image, image_type):
     small = None
     crops = requests.post( 'http://gip.rocks/v1',
                            data=image,
-                           headers={'Content-Type': image_type}
-            )
+                           headers={'Content-Type': image_type})
+
     if crops.status_code == 200:
         zf = zipfile.ZipFile(StringIO(crops.content))
         large = zf.open('160').read()
         small = zf.open('48').read()
+        return large, small
+    elif crops.status_code == 413:
+        raise ImageTooLarge
+    elif crops.status_code == 415:
+        raise InvalidImageType
+    else:
+        raise UnknownImageError
 
-    return crops.status_code, large, small
+class ImageTooLarge(Exception): pass
+
+class InvalidImageType(Exception): pass
+
+class UnknownImageError(Exception): pass
