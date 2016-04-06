6a1033123aede84d4a79ae5976e9c2b87d2b29d8
Chad Whitacre <chad@zetaweb.com>
2016-04-06 07:07:00 -0400

    Failing test for failing method whitelisting
    We're ending up with 500 instead of 405, because of a misordering in the
    algorithm functions.

--- tests/py/test_security.py
+++ tests/py/test_security.py
@@ -25,6 +25,9 @@ class TestSecurity(Harness):
             response = raises(Response, security.only_allow_certain_methods, request).value
             assert response.code == 405
 
+    def test_oacm_doesnt_choke_error_handling(self):
+        assert self.client.hit("OPTIONS", "/", raise_immediately=False).code == 405
+
 
     # ahtr - add_headers_to_response
 
