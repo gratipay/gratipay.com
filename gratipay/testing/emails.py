6132f5216971bbcb81a42f6794b4a37fb2bce0b2
Paul Kuruvilla <rohitpaulk@live.com>
2016-04-26 00:15:21 +0530

    Fix email tests

--- gratipay/testing/emails.py
+++ gratipay/testing/emails.py
@@ -8,7 +8,7 @@ class EmailHarness(Harness):
 
     def setUp(self):
         Harness.setUp(self)
-        self.mailer_patcher = mock.patch.object(Participant._mailer.messages, 'send')
+        self.mailer_patcher = mock.patch.object(Participant._mailer, 'send_email')
         self.mailer = self.mailer_patcher.start()
         self.addCleanup(self.mailer_patcher.stop)
         sleep_patcher = mock.patch('gratipay.models.participant.sleep')
@@ -16,4 +16,12 @@ class EmailHarness(Harness):
         self.addCleanup(sleep_patcher.stop)
 
     def get_last_email(self):
-        return self.mailer.call_args[1]['message']
+        args = self.mailer.call_args[1]
+
+        return {
+            'from': args['Source'],
+            'to': args['Destination']['ToAddresses'][0],
+            'subject': args['Message']['Subject']['Data'],
+            'body_text': args['Message']['Body']['Text']['Data'],
+            'body_html': args['Message']['Body']['Html']['Data']
+        }
