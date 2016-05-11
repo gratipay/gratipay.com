ba8c6e1a858c11f7fc8483ada20379a2044266f7
Chad Whitacre <chad@zetaweb.com>
2016-05-11 14:29:55 -0400

    Remove country names and code3s from database
    We're using names from the Babel library, and we're not using code3.

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,8 +1,6 @@
 CREATE TABLE countries -- http://www.iso.org/iso/country_codes
 ( id    bigserial   primary key
-, code2 text        NOT NULL UNIQUE
-, code3 text        NOT NULL UNIQUE
-, name  text        NOT NULL UNIQUE
+, code  text        NOT NULL UNIQUE
  );
 
 \i sql/countries.sql
