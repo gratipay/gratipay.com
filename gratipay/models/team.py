21f61d97b51493c4c80b1b099716bc44c6bcf4cc
Chad Whitacre <chad@zetaweb.com>
2016-06-08 14:29:23 -0400

    Add documentation for tip migration

--- gratipay/models/team.py
+++ gratipay/models/team.py
@@ -304,6 +304,15 @@ class Team(Model):
         }
 
     def migrate_tips(self):
+        """Migrate the Team owner's Gratipay 1.0 tips into 2.0 payment instructions to the Team.
+
+        :return: ``None``
+        :raises: :py:exc:`~gratipay.models.team.AlreadyMigrated` if payment
+            instructions already exist for this Team
+
+        This method gets called under :py:func:`migrate_all_tips` during payday.
+
+        """
         payment_instructions = self.db.all("""
             SELECT pi.*
               FROM payment_instructions pi
@@ -385,7 +394,19 @@ class Team(Model):
 def migrate_all_tips(db, print=print):
     """Migrate tips for all teams.
 
-    :param GratipayDatabase db:
+    :param GratipayDB db: a database object
+    :param func print: a function that takes lines of log output
+    :returns: ``None``
+
+    This function loads :py:class:`~gratipay.models.team.Team` objects for all
+    Teams where the owner had tips under Gratipay 1.0 but those tips have not
+    yet been migrated into payment instructions under Gratipay 2.0. It then
+    migrates the tips using :py:meth:`~gratipay.models.team.Team.migrate_tips`.
+
+    This function is wrapped in a script, ``bin/migrate-tips.py``, which is
+    `used during payday`_.
+
+    .. _used during payday: http://inside.gratipay.com/howto/run-payday
 
     """
     teams = db.all("""
@@ -412,4 +433,6 @@ def migrate_all_tips(db, print=print):
     print("Done.")
 
 
-class AlreadyMigrated(Exception): pass
+class AlreadyMigrated(Exception):
+    """Raised by :py:meth:`~gratipay.models.team.migrate_tips`.
+    """
