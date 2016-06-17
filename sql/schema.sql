0d22744ca73257e4c69e6d9d43683f7d241ffa19
Paul Kuruvilla <rohitpaulk@live.com>
2016-06-17 02:14:49 +0100

    merge branch.sql into schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -785,7 +785,28 @@ ALTER TABLE participant_identities ADD COLUMN is_verified boolean NOT NULL DEFAU
 ALTER TABLE participants ADD COLUMN has_verified_identity bool NOT NULL DEFAULT false;
 
 -- https://github.com/gratipay/gratipay.com/pull/4058
+
 ALTER TABLE payment_instructions ADD COLUMN participant_id bigint DEFAULT NULL
    REFERENCES participants(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE payment_instructions ADD COLUMN team_id bigint DEFAULT NULL
    REFERENCES teams(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+-- https://github.com/gratipay/gratipay.com/pull/4061
+
+DROP VIEW current_payment_instructions;
+
+UPDATE payment_instructions AS pi
+   SET participant_id = (SELECT id FROM participants p WHERE p.username = pi.participant)
+     , team_id = (SELECT id FROM teams t WHERE t.slug = pi.team);
+
+ALTER TABLE payment_instructions ALTER COLUMN participant_id SET NOT NULL;
+ALTER TABLE payment_instructions ALTER COLUMN team_id SET NOT NULL;
+
+CREATE VIEW current_payment_instructions AS
+    SELECT DISTINCT ON (participant_id, team_id) *
+      FROM payment_instructions
+  ORDER BY participant_id, team_id, mtime DESC;
+
+CREATE TRIGGER update_current_payment_instruction
+    INSTEAD OF UPDATE ON current_payment_instructions
+    FOR EACH ROW EXECUTE PROCEDURE update_payment_instruction();
