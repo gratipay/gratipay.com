4cfd33f33c51f81a9defa06a4513b5a4b978f1cf
Chad Whitacre <chad@zetaweb.com>
2016-07-01 12:01:42 -0400

    Start bringing back individual member endpoint

--- tests/py/test_www_team_distributing.py
+++ tests/py/test_www_team_distributing.py
@@ -15,11 +15,21 @@ class Tests(Harness):
         assert self.client.GET('/TheEnterprise/distributing/').code == 200
 
 
-    def test_distributing_json_redirects_when_no_money_is_available(self):
+    def test_json_redirects_when_no_money_is_available(self):
         self.make_team()
         assert self.client.GxT('/TheEnterprise/distributing/index.json').code == 302
 
-    def test_distributing_json_doesnt_redirect_when_money_is_available(self):
+    def test_json_doesnt_redirect_when_money_is_available(self):
         self.make_team()
         self.db.run("UPDATE teams SET available=537")
         assert self.client.GET('/TheEnterprise/distributing/index.json', raise_immediately=False).code == 500
+
+
+    def test_member_json_redirects_when_no_money_is_available(self):
+        self.make_team()
+        assert self.client.GxT('/TheEnterprise/distributing/1.json').code == 302
+
+    def test_member_json_doesnt_redirect_when_money_is_available(self):
+        self.make_team()
+        self.db.run("UPDATE teams SET available=537")
+        assert self.client.GET('/TheEnterprise/distributing/1.json', raise_immediately=False).code == 500
