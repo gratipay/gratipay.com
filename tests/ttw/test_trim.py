49abc81d24a9e86787dfaddd6cd374d274b954cf
Chad Whitacre <chad@zetaweb.com>
2016-07-11 07:40:10 -0400

    DRY up the trim tests

--- tests/ttw/test_trim.py
+++ tests/ttw/test_trim.py
@@ -6,13 +6,17 @@ from gratipay.testing import BrowserHarness
 
 class Tests(BrowserHarness):
 
+    def trim(self, val):
+        return self.js("window.Gratipay.trim('{}')".format(val))
+
+
     def test_trim_strips_all_unicode(self):
-        assert self.js("window.Gratipay.trim('˚aø¶')") == 'a'
-        assert self.js("window.Gratipay.trim('封b')") == 'b'
+        assert self.trim('˚aø¶') == 'a'
+        assert self.trim('封b') == 'b'
 
     def test_trim_strips_non_printable_ascii(self):
-        assert self.js("window.Gratipay.trim('\\n\\t\\rc')") == 'c'
+        assert self.trim('\\n\\t\\rc') == 'c'
 
     def test_trims_leading_and_trailing_whitespace(self):
-        assert self.js("window.Gratipay.trim('  foo bar ')") == 'foo bar'
-        assert self.js("window.Gratipay.trim('  foo  bar ')") == 'foo  bar'
+        assert self.trim('  foo bar ') == 'foo bar'
+        assert self.trim('  foo  bar ') == 'foo  bar'
