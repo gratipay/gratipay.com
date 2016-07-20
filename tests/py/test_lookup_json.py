3c8784be121d962982bc0a0c4f39c0ead8def027
nashe <thomas@chauchefoin.fr>
2016-07-20 21:57:39 +0200

    Respect non-searchable users choice when doing a lookup
    This aims to fix the report
    [H1-118699](https://hackerone.com/reports/118699): don't search using
    LIKE on the non-searchable users, but only display them in the list if
    it's an exact match.

--- tests/py/test_lookup_json.py
+++ tests/py/test_lookup_json.py
@@ -18,7 +18,25 @@ class TestLookupJson(Harness):
         assert len(data) == 1
         assert data[0]['id'] == -1
 
-    def test_get_existing_user(self):
+    def test_get_non_searchable_user_without_exact_match(self):
+        self.make_participant("alice", claimed_time=utcnow(), is_searchable=False)
+
+        response = self.client.GET('/lookup.json?query={}'.format('alic'))
+        data = json.loads(response.body)
+
+        assert len(data) == 1
+        assert data[0]['id'] == -1
+
+    def test_get_non_searchable_user_with_exact_match(self):
+        self.make_participant("alice", claimed_time=utcnow(), is_searchable=False)
+
+        response = self.client.GET('/lookup.json?query={}'.format('alice'))
+        data = json.loads(response.body)
+
+        assert len(data) == 1
+        assert data[0]['id'] != -1
+
+    def test_get_user_with_exact_match(self):
         self.make_participant("alice", claimed_time=utcnow())
 
         response = self.client.GET('/lookup.json?query={}'.format('alice'))
@@ -27,10 +45,12 @@ class TestLookupJson(Harness):
         assert len(data) == 1
         assert data[0]['id'] != -1
 
-    def test_get_stub_user(self):
-        self.make_participant("alice")
-        response = self.client.GET('/lookup.json?query={}'.format('a'))
+    def test_get_user_with_non_exact_match(self):
+        self.make_participant("alice", claimed_time=utcnow())
+
+        response = self.client.GET('/lookup.json?query={}'.format('alic'))
         data = json.loads(response.body)
 
-        assert len(data) == 1
-        assert data[0]['id'] == -1
+        assert len(data) == 2
+        assert data[0]['id'] != -1
+        assert data[1]['id'] == -1
