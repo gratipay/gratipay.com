64703a7437babe2e96ea318d522abad9165a97c5
Chad Whitacre <chad@zetaweb.com>
2016-08-03 17:55:19 -0400

    Return only valid team members from lookup.json

--- tests/py/test_lookup_json.py
+++ tests/py/test_lookup_json.py
@@ -11,6 +11,11 @@ class TestLookupJson(Harness):
         data = json.loads(response.body)
         return [d['id'] for d in data]
 
+
+    def make_alice(self, **kw):
+        kw.update(claimed_time='now', email_address='alice@example.com', verified_in='TT')
+        return self.make_participant("alice", **kw)
+
     def test_get_without_query_returns_400(self):
         response = self.client.GxT('/lookup.json')
         assert response.code == 400
@@ -19,19 +24,19 @@ class TestLookupJson(Harness):
         assert self.lookup('alice') == [-1]
 
     def test_looking_up_non_searchable_user_with_an_exact_match_finds_them(self):
-        alice = self.make_participant("alice", claimed_time='now', is_searchable=False)
+        alice = self.make_alice(is_searchable=False)
         assert self.lookup('alice') == [alice.id]
 
     def test_looking_up_non_searchable_user_without_exact_match_finds_nothing(self):
-        self.make_participant("alice", claimed_time='now', is_searchable=False)
+        self.make_alice(is_searchable=False)
         assert self.lookup('alic') == [-1]
 
     def test_looking_up_searchable_user_with_an_exact_match_finds_them(self):
-        alice = self.make_participant("alice", claimed_time='now')
+        alice = self.make_alice()
         assert self.lookup('alice') == [alice.id]
 
     def test_looking_up_searchable_user_without_an_exact_match_finds_them(self):
-        alice = self.make_participant("alice", claimed_time='now')
+        alice = self.make_alice()
         assert self.lookup('alic') == [alice.id, -1]
 
 
@@ -42,16 +47,16 @@ class TestLookupJson(Harness):
                              , email_address='alice@example.com'
                              , is_suspicious=True
                               )
-        assert self.lookup('alice') == []
+        assert self.lookup('alice') == [-1]
 
     def test_looking_up_unverified_user_finds_nothing(self):
         self.make_participant("alice", claimed_time='now', email_address='alice@example.com')
-        assert self.lookup('alice') == []
+        assert self.lookup('alice') == [-1]
 
     def test_looking_up_user_with_no_email_finds_nothing(self):
         self.make_participant("alice", claimed_time='now')  # can't verify w/o email!
-        assert self.lookup('alice') == []
+        assert self.lookup('alice') == [-1]
 
     def test_looking_up_unclaimed_user_finds_nothing(self):
         self.make_participant("alice", verified_in='TT', email_address='alice@example.com')
-        assert self.lookup('alice') == []
+        assert self.lookup('alice') == [-1]
