14a5f24ad56f531dc04993cbfda1307ef4e37521
Abhishek Raj <abhishekrm@iitrpr.ac.in>
2016-08-06 15:30:12 +0530

    Remove previous click handlers from confirm events.

--- js/gratipay/confirm.js
+++ js/gratipay/confirm.js
@@ -2,8 +2,8 @@ Gratipay.confirm = function(message, yes, no) {
     var $m = $('.modal');
     $m.show();
     $('.confirmation-message', $m).html(message);
-    $('.yes', $m).click(function() { yes(); Gratipay.confirm.close(); });
-    $('.no', $m).click(function() { no(); Gratipay.confirm.close(); });
+    $('.yes', $m).off('click').click(function() { yes(); Gratipay.confirm.close(); });
+    $('.no', $m).off('click').click(function() { no(); Gratipay.confirm.close(); });
 };
 
 Gratipay.confirm.close = function() {
