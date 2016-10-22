e43f8153791967591279de29c5520fd0454ab8b6
Chad Whitacre <chad@zetaweb.com>
2016-10-21 20:08:11 -0400

    Stub out marky-markdown

--- gratipay/utils/markdown.py
+++ gratipay/utils/markdown.py
@@ -1,5 +1,8 @@
+import subprocess
+
 from markupsafe import Markup
 import misaka as m  # http://misaka.61924.nl/
+
 
 def render(markdown):
     return Markup(m.html(
@@ -7,3 +10,9 @@ def render(markdown):
         extensions=m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH | m.EXT_NO_INTRA_EMPHASIS,
         render_flags=m.HTML_SKIP_HTML | m.HTML_TOC | m.HTML_SMARTYPANTS | m.HTML_SAFELINK
     ))
+
+
+def marky(markdown):
+    """Process markdown the same way npm does.
+    """
+    return subprocess.call()
