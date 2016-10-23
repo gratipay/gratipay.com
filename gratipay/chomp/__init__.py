43c5831cb8a33002f98549621fae8261c76cb34c
aandis <abhishekrm@iitrpr.ac.in>
2016-10-23 20:05:29 +0530

    Create npm class and modify insert method.

--- gratipay/chomp/__init__.py
+++ gratipay/chomp/__init__.py
@@ -4,6 +4,7 @@ The main function is exposed as a console script named `chomp` via setup.py.
 
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
+from collections import OrderedDict
 
 import argparse
 import sys
@@ -15,50 +16,68 @@ import requests
 from gratipay.utils import markdown
 from io import StringIO
 
-def from_npm(package, package_id):
-    """Given an npm package dict, return a dict of info and a list of emails.
+class NPM(object):
+    """Represent npm.
     """
-    out = {}
-    out['id'] = str(package_id)
-    out['name'] = package['name']
-    out['description'] = package['description']
-    out['package_manager_id'] = "1" # Todo - generalize
-    out['long_description'] = 'null'
-    out['long_description_raw'] = 'null'
-    out['long_description_type'] = 'null'
+    def __init__(self):
+        self.name = 'npm'
 
-    emails = []
-    for key in ('authors', 'maintainers'):
-        for person in package.get(key, []):
-            if type(person) is dict:
-                email = person.get('email')
-                if email:
-                    emails.append({ 'package_id': str(package_id), 'email': email })
+    def parse(self, package, package_manager_id, package_id):
+        """Given an npm package dict, return a dict of info and a list of emails to be imported.
+        """
+        out = OrderedDict()
+        out['id'] = str(package_id)
+        out['package_manager_id'] = str(package_manager_id)
+        out['name'] = package['name']
+        out['description'] = package['description'] if 'description' in package else ''
 
-    return out, emails
+        emails = []
+        for key in ('authors', 'maintainers'):
+            for person in package.get(key, []):
+                if type(person) is dict:
+                    email = person.get('email')
+                    if email:
+                        emails.append(OrderedDict([
+                            ('package_id', str(package_id)),
+                            ('email', email)
+                        ]))
+
+        return out, emails
+
+    def fetch_catalog(self):
+        r = requests.get('https://registry.npmjs.com/-/all', verify=False)
+        r.raise_for_status()
+        return r.json()
+
 
 def stringify(obj):
     return '\t'.join([v for k, v in obj.iteritems()])
 
-def insert_catalog(catalog):
-    package_id = 0
-    package_data = StringIO()
-    email_data = StringIO()
-    for k, v in catalog.iteritems():
-        package_id += 1
-        package, emails = from_npm(v, package_id)
-        package_data.write(stringify(package) + '\n')
-        for email in emails:
-            email_data.write(stringify(email) + '\n')
+def insert_catalog_for(pm):
+    catalog = pm.fetch_catalog()
+    conn = psycopg2.connect(host='localhost', database='gratipay')
+    if catalog:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO package_managers (name)
+                VALUES (%s) RETURNING id
+            """, (pm.name,))
+        package_manager_id = cursor.fetchone()[0]
+        package_id = 0 # Mass assign ids so they are usable during email insertion
+        package_data = StringIO()
+        email_data = StringIO()
+        for k, v in catalog.iteritems():
+            package_id += 1
+            package, emails = pm.parse(v, package_manager_id, package_id)
+            package_data.write(stringify(package) + '\n')
+            for email in emails:
+                email_data.write(stringify(email) + '\n')
+        package_data.seek(0)
+        email_data.seek(0)
 
-
-def fetch_catalog():
-    # r = requests.get('https://registry.npmjs.com/-/all', verify=False)
-    # r.raise_for_status()
-    # return r.json()
-    import json
-    return json.loads('{"a11y-announcer":{"name":"a11y-announcer","description":"An accessible ember route change announcer","dist-tags":{"latest":"1.0.2"},"maintainers":[{"name":"robdel12","email":"Robertdeluca19@gmail.com"}],"homepage":"https://github.com/ember-a11y/a11y-announcer#readme","keywords":["ember-addon","ember accessibility","ember router","a11y-announcer"],"repository":{"type":"git","url":"git+https://github.com/ember-a11y/a11y-announcer.git"},"author":{"name":"Robert DeLuca"},"bugs":{"url":"https://github.com/ember-a11y/a11y-announcer/issues"},"license":"MIT","readmeFilename":"README.md","users":{"jalcine":true,"unwiredbrain":true},"time":{"modified":"2016-08-13T23:03:37.135Z"},"versions":{"1.0.2":"latest"}}}')
-
+        cursor.copy_from(package_data, 'packages', columns=["id", "package_manager_id", "name", "description"])
+        cursor.copy_from(email_data, 'package_emails', columns=["package_id", "email"])
+    conn.commit()
 
 def update_database(SQL):
     pass
@@ -69,9 +88,14 @@ def parse_args(argv):
     p.add_argument( 'if_modified_since'
                   , help='a number of minutes in the past, past which we need new updates'
                    )
+    p.add_argument('package_manager'
+                  , help='the name of the package manager to import from'
+                   )
     return p.parse_args(argv)
 
 
 def main(argv=sys.argv):
-    ims = parse_args(argv[1:]).if_modified_since
-    insert_catalog(fetch_catalog())
+    pm_arg = parse_args(argv[1:]).package_manager
+    if pm_arg == 'npm':
+        pm = NPM()
+    insert_catalog_for(pm)
