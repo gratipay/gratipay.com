e201a3c8a07be0f0e8eff88972b5a2601fe7b87d
Chad Whitacre <chad@zetaweb.com>
2016-10-24 16:44:38 -0400

    Can we just call it readme? Let's call it readme.

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,14 +1,14 @@
 BEGIN;
 
     CREATE TABLE packages
-    ( id                    bigserial                   PRIMARY KEY
-    , package_manager       text                        NOT NULL
-    , name                  text                        NOT NULL
-    , description           text                        NOT NULL
-    , long_description      text                        NOT NULL DEFAULT ''
-    , long_description_raw  text                        NOT NULL DEFAULT ''
-    , long_description_type text                        NOT NULL DEFAULT ''
-    , emails                text[]                      NOT NULL
+    ( id                bigserial   PRIMARY KEY
+    , package_manager   text        NOT NULL
+    , name              text        NOT NULL
+    , description       text        NOT NULL
+    , readme            text        NOT NULL DEFAULT ''
+    , readme_raw        text        NOT NULL DEFAULT ''
+    , readme_type       text        NOT NULL DEFAULT ''
+    , emails            text[]      NOT NULL
     , UNIQUE (package_manager, name)
      );
 
