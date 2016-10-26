7f519c1eab146fba4762d3ea56f3e32ddf34905a
Chad Whitacre <chad@zetaweb.com>
2016-10-26 09:47:20 -0400

    Fiddle with checksum check

--- bin/sync-npm.sh
+++ bin/sync-npm.sh
@@ -8,7 +8,8 @@ pip install -e .
 pip install ijson==2.3.0
 
 curl https://cmake.org/files/v3.6/cmake-3.6.2-Linux-x86_64.tar.gz > cmake.tgz
-test sha256sum cmake.tgz = 5df4b69d9e85093ae78b1070d5cb9f824ce0bdd02528948c3f6a740e240083e5
+test echo '5df4b69d9e85093ae78b1070d5cb9f824ce0bdd02528948c3f6a740e240083e5  cmake.tgz' \
+     | sha256sum cmake.tgz -c /dev/stdin
 tar zxf cmake.tgz
 PATH=/app/cmake-3.6.2-Linux-x86_64/bin:$PATH
 
