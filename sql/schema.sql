1f6b9937b7b848e39946bb1a2f37b8f2df2fd218
Chad Whitacre <chad@zetaweb.com>
2016-10-26 10:41:11 -0400

    merge branch.sql into schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -862,3 +862,21 @@ BEGIN;
         ) AS anon WHERE amount > 0;
 
 END;
+
+
+-- https://github.com/gratipay/gratipay.com/pull/4153
+BEGIN;
+
+    CREATE TABLE packages
+    ( id                bigserial   PRIMARY KEY
+    , package_manager   text        NOT NULL
+    , name              text        NOT NULL
+    , description       text        NOT NULL
+    , readme            text        NOT NULL DEFAULT ''
+    , readme_raw        text        NOT NULL DEFAULT ''
+    , readme_type       text        NOT NULL DEFAULT ''
+    , emails            text[]      NOT NULL
+    , UNIQUE (package_manager, name)
+     );
+
+END;
