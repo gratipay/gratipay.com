d7f640ee13962dd4dc32e0e1ef0623b3692481d9
Chad Whitacre <chad@zetaweb.com>
2016-10-26 22:14:33 -0400

    Oop! Delete branch.sql
    I redid the sql-merging commit locally and neglected to remove this
    file.

--- sql/branch.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-    ALTER TABLE packages ALTER COLUMN readme_raw DROP NOT NULL;
-    ALTER TABLE packages ALTER COLUMN readme_raw SET DEFAULT NULL;
-    UPDATE packages SET readme_raw=NULL;
-END;
