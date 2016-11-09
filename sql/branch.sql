3729efbbb30742adfcf80b5ec8820f31a83f3899
Chad Whitacre <chad@zetaweb.com>
2016-11-08 18:04:32 -0800

    We don't need to modify the readme column anymore
    We're using readme_needs_to_be_updated instead.

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,6 +1,1 @@
-BEGIN;
-    ALTER TABLE packages ALTER COLUMN readme DROP NOT NULL;
-    ALTER TABLE packages ALTER COLUMN readme SET DEFAULT NULL;
-    UPDATE packages SET readme=NULL;
-    ALTER TABLE packages ADD COLUMN readme_needs_to_be_processed bool NOT NULL DEFAULT true;
-END;
+ALTER TABLE packages ADD COLUMN readme_needs_to_be_processed bool NOT NULL DEFAULT true;
