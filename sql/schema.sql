cbdf50fa330b9108dae5e73d948a27f3b96e8d0d
Chad Whitacre <chad@zetaweb.com>
2016-11-23 17:27:17 -0500

    merge branch.sql into schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -892,3 +892,37 @@ END;
 
 -- https://github.com/gratipay/gratipay.com/pull/4164
 ALTER TABLE packages ADD COLUMN readme_needs_to_be_processed bool NOT NULL DEFAULT true;
+
+
+-- https://github.com/gratipay/gratipay.com/pull/3975
+
+-- Alter the enums to cater for missing data.
+ALTER TYPE payment_net ADD VALUE 'unknown';
+ALTER TYPE exchange_status ADD VALUE 'unknown';
+
+-- Update the field status in the exchanges table from NULL to 'unknown'
+UPDATE exchanges SET status = 'unknown' WHERE status IS NULL;
+
+-- Alter the exchanges table to ensure that no more NULL values are entered
+ALTER TABLE exchanges ALTER COLUMN status SET NOT NULL;
+
+-- Insert records for 'unknown' (previously NULL) in exchanges table
+-- network in exchange_route table
+INSERT INTO exchange_routes (participant, network, address, error)
+     (
+       SELECT DISTINCT participants.id, 'unknown'::payment_net, 'n/a', ''
+       FROM exchanges, participants
+       WHERE exchanges.participant = participants.username
+       AND route IS NULL
+     );
+
+-- Update exchanges records with exchange_route ids pointing to 'unknown'
+-- network records for that participants
+UPDATE exchanges
+SET route = exchange_routes.id
+FROM exchange_routes, participants
+WHERE exchange_routes.participant = participants.id
+AND participants.username = exchanges.participant;
+
+-- Alter exchanges table and set route to not null
+ALTER TABLE exchanges ALTER COLUMN route SET NOT NULL;
