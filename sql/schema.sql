f5cdf18839636a479fb3bac54dc0568851770e88
Chad Whitacre <chad@zetaweb.com>
2017-01-09 16:16:43 -0500

    merge branch.sql into schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -941,3 +941,19 @@ END;
 BEGIN;
   ALTER TABLE teams DROP COLUMN todo_url;
 END;
+
+
+--https://github.com/gratipay/gratipay.com/pull/4266
+BEGIN;
+
+    /* Add a new column to hold the scrubbed content. */
+    ALTER TABLE statements ADD COLUMN content_scrubbed text NOT NULL DEFAULT '';
+
+    /* Modify the existing search_vector_update trigger to operate on the new column. */
+    DROP TRIGGER search_vector_update ON statements;
+    CREATE TRIGGER search_vector_update
+        BEFORE INSERT OR UPDATE ON statements
+        FOR EACH ROW EXECUTE PROCEDURE
+        tsvector_update_trigger_column(search_vector, search_conf, content_scrubbed);
+
+END;
