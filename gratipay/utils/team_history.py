874ceb7a877553b387bc3bf70566b33514766965
Kwesi Aguillera <kaguillera@gmail.com>
2017-02-02 20:01:49 -0500

    Simplified the SQL query anot output drastically

--- gratipay/utils/team_history.py
+++ gratipay/utils/team_history.py
@@ -40,89 +40,31 @@ def iter_payday_events(db, team, year=None):
     current_year = datetime.utcnow().year
     year = year or current_year
 
-    paydays = db.all("""
-        SELECT id, ts_start::date
-          FROM paydays
-         WHERE ts_start > %(ctime)s
-           AND extract(year from ts_start) = %(year)s
-      ORDER BY id DESC
-    """, dict(ctime=team.ctime,year=year), back_as=dict)
+    payments = db.all("""
+        SELECT payments.*, paydays.ts_start
+          FROM payments, paydays
+         WHERE payments.payday = paydays.id
+           AND team=%(team)s
+           AND extract(year from timestamp) = %(year)s
+         ORDER BY payments.payday
+       """, dict(team=team.name, year=year), back_as=dict)
 
     events = []
-    events_query = """
-        SELECT *
-          FROM ( SELECT COALESCE( actual.amount,expected.amount) as amount
-                      , COALESCE( expected.participant_id, actual.participant_id ) as participant_id
-                      , ( CASE WHEN is_funded IS false then 'No/Invalid Credit Card' end) as Notes
-                      , COALESCE( actual.participant, expected.participant) as participant
-                      , actual.team
-                      , COALESCE(actual.direction, 'to-participant') AS direction
-                      , ( CASE when actual.amount is NULL then 'failed' else 'Succeeded' end) as Status
-                   FROM ( SELECT DISTINCT ON (participant_id) payment_instructions.*
-                             , participants.username as participant
-                             , teams.name as team
-                         FROM payment_instructions, participants, teams
-                        WHERE is_funded = true
-                          AND mtime < %(payday_date)s
-                          AND team_id = ( SELECT id
-                                            FROM teams
-                                            WHERE name = %(team)s  )
-                          AND participants.id = participant_id
-                          AND teams.id = team_id
-                        ORDER BY participant_id, team_id, mtime DESC
-                       ) expected
-                   FULL OUTER JOIN ( SELECT payments.*
-                                          , participants.id as participant_id
-                                       FROM payments, participants
-                                      WHERE payday = %(payday_id)s
-                                        AND direction = 'to-team'
-                                        AND team = %(team)s
-                                        AND participants.username = participant
-                                   ) actual
-                     ON expected.participant_id = actual.participant_id
-                ) received
-         UNION ( SELECT COALESCE( actual.amount,expected.amount) as amount
-                      , COALESCE( expected.participant_id, actual.participant_id ) as participant_id
-                      , ( CASE WHEN expected.amount IS NULL
-                          THEN 'Owner Payout'
-                          WHEN expected.amount > actual.amount
-                          THEN 'Not Enough Funds'
-                          ELSE '' END
-                        ) as Notes
-                      , COALESCE( actual.participant, expected.participant) as participant
-                      , actual.team
-                      , COALESCE(actual.direction, 'to-team') AS direction
-                      , (CASE when actual.amount is NULL then 'failed' else 'Succeeded' end) as Status
-                   FROM ( SELECT DISTINCT ON (participant_id) takes.*
-                               , participants.username as participant
-                               , teams.name as team
-                            FROM takes, participants, teams
-                           WHERE mtime < %(payday_date)s
-                             AND team_id = ( SELECT id
-                                               FROM teams
-                                              WHERE name = %(team)s )
-                             AND participants.id = participant_id
-                             AND teams.id = team_id
-                           ORDER BY participant_id, team_id, mtime DESC
-                        ) expected
-                   FULL OUTER JOIN ( SELECT payments.*
-                                          , participants.id as participant_id
-                                       FROM payments, participants
-                                      WHERE payday = %(payday_id)s
-                                        AND direction = 'to-participant'
-                                        AND team = %(team)s
-                                        AND participants.username = participant
-                                    ) actual
-                   ON expected.participant_id = actual.participant_id )
-         ORDER BY direction"""
+    payday_id = payments[0]['payday']
+    payday_date = payments[0]['ts_start']
+    payday_events = []
 
-    for payday in paydays:
-        payday_events = db.all(events_query
-                              , dict(team=team.name
-                                    , payday_id=payday['id']
-                                    , payday_date=payday['ts_start'])
-                              , back_as=dict)
+    #return payments
 
-        events.append (dict(id=payday['id'], date=payday['ts_start'], events=payday_events))
+    for payment in payments:
+        if payment['payday'] != payday_id:
+            events.append (dict(id=payday_id, date=payment['ts_start'], events=payday_events))
+            payday_id = payment['payday']
+            payday_date = payment['ts_start']
+            payday_events = []
+
+        payday_events.append(payment)
+
+    events.append (dict(id=payday_id, date=payment['ts_start'], events=payday_events))
 
     return events
