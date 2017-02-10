269e90ea4a0f14fb02cd8af3045feb80cbd94174
Chad Whitacre <chad@zetaweb.com>
2017-02-10 11:04:15 -0500

    Extend the ACME challenge system
    - mount read-only under /assets/
    - display a count for admins on ./

--- gratipay/utils/__init__.py
+++ gratipay/utils/__init__.py
@@ -42,7 +42,8 @@ def _munge(website, request, url_prefix, fs_prefix):
         request.path.__init__(fs_prefix + request.path.raw[len(url_prefix):])
 
 def help_aspen_find_well_known(website, request):
-    return _munge(website, request, '/.well-known/', '/_well-known/')
+    _munge(website, request, '/.well-known/', '/_well-known/')
+    _munge(website, request, '/assets/.well-known/', '/assets/_well-known/')
 
 def use_tildes_for_participants(website, request):
     return _munge(website, request, '/~', '/~/')
