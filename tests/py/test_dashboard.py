d9a73b905185ecf04f13ef8a3d2bbfa50767eca1
Chad Whitacre <chad@zetaweb.com>
2017-03-02 12:01:03 -0500

    Clean up test suite for nmasspays

--- tests/py/test_dashboard.py
+++ tests/py/test_dashboard.py
@@ -6,42 +6,41 @@ from gratipay.testing.billing import BillingHarness
 
 class TestNMassPays(BillingHarness):
 
-    # nmp - nmasspays
-
     def setUp(self):
         BillingHarness.setUp(self)
         self.make_participant('admin', claimed_time='now', is_admin=True).username
+        team = self.make_team(owner=self.homer, is_approved=True)
+        self.obama.set_payment_instruction(team, '20.00')
 
-    def post_masspay(self, username, route, amount):
-        self.client.PxST( '/~{}/history/record-an-exchange'.format(username)
-                        , { 'amount': unicode(amount)
+    def post_masspay(self):
+        self.client.PxST( '/~homer/history/record-an-exchange'
+                        , { 'amount': '20'
                           , 'fee': '0'
                           , 'note': 'Exchange!'
                           , 'status': 'succeeded'
-                          , 'route_id': unicode(route.id)
+                          , 'route_id': unicode(self.homer_route.id)
                            }
                         , auth_as='admin'
                          )  # responds with 302
 
 
-    def test_nmp_returns_zero_for_no_paydays(self):
-        self.client.GET('/dashboard/nmasspays').body == '0'
+    def test_returns_zero_for_no_paydays(self):
+        assert self.client.GET('/dashboard/nmasspays').body == '0'
 
-    def test_nmp_returns_zero_for_no_masspays(self):
+    def test_returns_zero_for_one_payday(self):
         self.run_payday()
-        self.client.GET('/dashboard/nmasspays').body == '0'
+        assert self.client.GET('/dashboard/nmasspays').body == '0'
 
-    def test_nmp_returns_one_for_one_payday_with_one_masspay(self):
-        team = self.make_team(owner=self.homer, is_approved=True)
-        self.obama.set_payment_instruction(team, '20.00')
+    def test_returns_zero_for_penultimate_payday_with_no_masspays(self):
+        self.run_payday(); self.post_masspay()
         self.run_payday()
-        self.post_masspay('homer', self.homer_route, 20)
-        self.client.GET('/dashboard/nmasspays').body == '1'
+        self.run_payday(); self.post_masspay()
+        assert self.client.GET('/dashboard/nmasspays').body == '0'
 
-    def test_nmp_returns_one_for_many_paydays_with_one_masspay(self):
-        team = self.make_team(owner=self.homer, is_approved=True)
-        self.obama.set_payment_instruction(team, '20.00')
-        for i in range(10):
-            self.run_payday()
-            self.post_masspay('homer', self.homer_route, 20)
-        self.client.GET('/dashboard/nmasspays').body == '1'
+    def test_returns_one_for_penultimate_payday_with_one_masspay(self):
+        self.run_payday(); self.post_masspay()
+        self.run_payday(); self.post_masspay()
+        self.run_payday()
+        self.run_payday(); self.post_masspay()
+        self.run_payday()
+        assert self.client.GET('/dashboard/nmasspays').body == '1'
