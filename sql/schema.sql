056c0fb1d8c1873c1fd85c773ddfa041c365b8dc
Chad Whitacre <chad@zetaweb.com>
2017-03-02 13:46:49 -0500

    merge branch.sql into schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -960,3 +960,22 @@ END;
 BEGIN;
     CREATE TABLE acme_challenges (token text, "authorization" text);
 END;
+
+
+-- https://github.com/gratipay/gratipay.com/pull/4170
+BEGIN;
+    ALTER TABLE exchange_routes ADD COLUMN is_deleted bool NOT NULL DEFAULT FALSE;
+
+    DROP CAST (current_exchange_routes AS exchange_routes);
+    DROP VIEW current_exchange_routes;
+
+    CREATE VIEW current_exchange_routes AS
+        SELECT DISTINCT ON (participant, network) *
+          FROM exchange_routes
+         WHERE NOT is_deleted
+      ORDER BY participant, network, id DESC;
+
+    CREATE CAST (current_exchange_routes AS exchange_routes) WITH INOUT;
+
+    UPDATE exchange_routes SET is_deleted = true, error = '' WHERE error = 'invalidated';
+END;
