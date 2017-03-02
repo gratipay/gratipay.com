4bafcf911f8accb2ff8afb27c398ac626eef1855
Kwesi Aguillera <kaguillera@gmail.com>
2017-03-02 16:11:30 -0500

    Fixed the make_exchange in harness that uses record_exchange

--- gratipay/utils/fake_data.py
+++ gratipay/utils/fake_data.py
@@ -220,6 +220,8 @@ def fake_payment(db, participant, team, timestamp, amount, payday, direction):
 
 
 def fake_exchange(db, participant, amount, fee, timestamp):
+    route=get_exchange_route(db, participant.id)
+    ref = '%s-%s-%s' % (participant.username, route, timestamp.microsecond)
     return insert_fake_data( db
                            , "exchanges"
                            , timestamp=timestamp
@@ -227,7 +229,8 @@ def fake_exchange(db, participant, amount, fee, timestamp):
                            , amount=amount
                            , fee=fee
                            , status='succeeded'
-                           , route=get_exchange_route(db, participant.id)
+                           , route=route
+                           , ref=ref
                             )
 
 
