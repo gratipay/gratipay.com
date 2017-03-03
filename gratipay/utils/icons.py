8b08f7696e51631855f8d2d2710aa37de0e4431c
mattbk <bedrocks@gmail.com>
2017-03-03 15:13:06 -0600

    Partially factor out status icons.

---
+++ gratipay/utils/icons.py
@@ -0,0 +1,10 @@
+
+
+def statusicons():
+    status_icons = { "approved": "&#xe008;"
+                   , "unreviewed": "&#xe009;"
+                   , "rejected": "&#xe010;"
+                   , "featured": "&#xe9d9;"
+                    }
+
+    return status_icons
