b198c7f193901e52318b24a137202dd813123359
Matt <bedrocks@gmail.com>
2017-03-04 09:38:42 -0600

    Change from function to variable.

--- gratipay/utils/icons.py
+++ gratipay/utils/icons.py
@@ -1,10 +1,5 @@
-
-
-def statusicons():
-    status_icons = { "approved": "&#xe008;"
+STATUS_ICONS = { "approved": "&#xe008;"
                    , "unreviewed": "&#xe009;"
                    , "rejected": "&#xe010;"
                    , "featured": "&#xe9d9;"
                     }
-
-    return status_icons
