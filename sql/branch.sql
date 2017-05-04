eef21c04e151c11662054d7fee99ce5f58a13465
Chad Whitacre <chad@zetaweb.com>
2017-05-04 10:06:08 -0400

    merge branch.sql into schema.sql

--- sql/branch.sql
+++
@@ -1,3 +0,0 @@
--- A bug was discovered in https://github.com/gratipay/gratipay.com/pull/4439
--- Let's delete all cached entries in the table, they'll be regenerated with proper values.
-DELETE FROM balances_at;
