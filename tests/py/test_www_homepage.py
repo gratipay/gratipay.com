2175aa20bf647ec1844941b018c2e4e0f116e579
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-05-23 00:51:26 +0530

    Support 'page' query param on homepage

--- tests/py/test_www_homepage.py
+++ tests/py/test_www_homepage.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from gratipay.models.package import NPM, Package
 from gratipay.testing import Harness
 
 
 class Tests(Harness):
 
-	def test_filter_by_approved(self):
+    # Filtering
+
+    def test_filter_by_approved(self):
         self.make_team(is_approved=True)
         assert 'The Enterprise' in self.client.GET("/?status=approved").body
 
@@ -18,3 +19,40 @@ class Tests(Harness):
     def test_filter_by_rejected(self):
         self.make_team(is_approved=False)
         assert 'The Enterprise' in self.client.GET("/?status=rejected").body
+
+    # Pagination
+
+    def test_first_page_is_shown_by_default(self):
+        for i in xrange(1, 21):
+            self.make_team(is_approved=True, name='Gratiteam ' + str(i))
+
+        homepage = self.client.GET("/?status=approved").body
+
+        assert 'Gratiteam 20' in homepage
+        assert 'Gratiteam 11' in homepage
+        assert 'Gratiteam 10' not in homepage
+        assert 'Gratiteam 9' not in homepage
+
+    def test_page_filter_works(self):
+        for i in xrange(1, 21):
+            self.make_team(is_approved=True, name='Gratiteam ' + str(i))
+
+        homepage = self.client.GET("/?status=approved&page=2").body
+
+        assert 'Gratiteam 20' not in homepage
+        assert 'Gratiteam 11' not in homepage
+        assert 'Gratiteam 10' in homepage
+        assert 'Gratiteam 9'  in homepage
+
+    def test_invalid_page_filter_does_not_blow_up(self):
+        self.client.GET("/?status=approved&page=abcd") # Should not throw an exception
+
+    def test_out_of_bounds_page_filter_renders_zero_projects(self):
+        for i in xrange(1, 5):
+            self.make_team(is_approved=True, name='Gratiteam ' + str(i))
+
+        homepage = self.client.GET("/?status=approved&page=500").body
+
+        assert 'Gratiteam' not in homepage
+
+
