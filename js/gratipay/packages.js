1b1da812f703bb7a1d3c35c2fe71c475f4dbc7be
Chad Whitacre <chad@zetaweb.com>
2017-05-26 11:09:04 -0400

    Implement bulk claiming on /on/npm/

--- js/gratipay/packages.js
+++ js/gratipay/packages.js
@@ -1,33 +1,60 @@
 Gratipay.packages = {};
 
-Gratipay.packages.init = function() {
-    Gratipay.Select('.gratipay-select');
-    $('button.apply').on('click', Gratipay.packages.post);
+Gratipay.packages.initBulk = function() {
+    $('button.apply').on('click', Gratipay.packages.postBulk);
 };
 
-Gratipay.packages.post = function(e) {
+Gratipay.packages.initSingle = function() {
+    Gratipay.Select('.gratipay-select');
+    $('button.apply').on('click', Gratipay.packages.postOne);
+};
+
+
+Gratipay.packages.postBulk = function(e) {
     e.preventDefault();
-    var $this = $(this);
-    var action = 'start-verification';
-    var package_id = $('input[name=package_id]').val();
+    var pkg, email, package_id, package_ids_by_email={};
+    $('table.listing td.item ').not('.disabled').each(function() {
+        pkg = $(this).data();
+        if (package_ids_by_email[pkg.email] === undefined)
+            package_ids_by_email[pkg.email] = [];
+        package_ids_by_email[pkg.email].push(pkg.packageId);
+    });
+    for (email in package_ids_by_email)
+        Gratipay.packages.post(email, package_ids_by_email[email], true);
+};
+
+Gratipay.packages.postOne = function(e) {
+    e.preventDefault();
     var email = $('input[name=email]:checked').val();
+    var package_id = $('input[name=package_id]').val();
+    Gratipay.packages.post(email, [package_id]);
+}
 
-    var $inputs = $('input, button');
-    $inputs.prop('disabled', true);
 
+Gratipay.packages.post = function(email, package_ids, show_email) {
+    var action = 'start-verification';
+    var $button = $('button.apply')
+
+    $button.prop('disabled', true);
+    function reenable() { $button.prop('disabled', false); }
     $.ajax({
         url: '/~' + Gratipay.username + '/emails/modify.json',
         type: 'POST',
-        data: {action: action, address: email, package_id: package_id},
+        data: { action: action
+              , address: email
+              , package_id: package_ids
+              , show_address_in_message: true
+               },
+        traditional: true,
         dataType: 'json',
         success: function (msg) {
             if (msg) {
                 Gratipay.notification(msg, 'success');
+                reenable();
             }
-            $inputs.prop('disabled', false);
         },
         error: [
-            function () { $inputs.prop('disabled', false); },
+            reenable,
             Gratipay.error
         ],
     });
