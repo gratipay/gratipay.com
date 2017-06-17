283c2ea17a8f58548e63fcafbacc946172d5bf13
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-06-17 16:39:33 +0530

    Remove grayout, reuse modalWrapper

--- js/gratipay/modal.js
+++ js/gratipay/modal.js
@@ -1,17 +1,21 @@
 Gratipay.modal = {}
 
 Gratipay.modal.open = function(modalWrapper) {
-    $('body').append('<div id="modal-grayout"></div>');
     $(modalWrapper).show().focus();
 
     var closeModal = function() {
         Gratipay.modal.close(modalWrapper);
     };
-    $('#modal-grayout').click(closeModal);
+    $(modalWrapper).on('click', function (e) {
+        if (e.target === this) {
+            closeModal();
+        } else {
+            return; // Don't close modal if the click was on descendants
+        }
+    });
     $(modalWrapper).find('button.close-modal').click(closeModal);
 };
 
 Gratipay.modal.close = function(modalWrapper) {
     $(modalWrapper).hide();
-    $('#modal-grayout').remove();
 };
