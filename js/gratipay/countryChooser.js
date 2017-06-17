3739e11249b2644fd2e373f6151fa96b226dcc37
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-06-17 13:34:18 +0530

    Extract grayout into modal.scss

--- js/gratipay/countryChooser.js
+++ js/gratipay/countryChooser.js
@@ -3,16 +3,16 @@ Gratipay.countryChooser = {}
 Gratipay.countryChooser.init = function() {
     $('.open-country-chooser').click(Gratipay.countryChooser.open);
     $('.close-country-chooser').click(Gratipay.countryChooser.close);
-    $('#grayout').click(Gratipay.countryChooser.close);
 };
 
 Gratipay.countryChooser.open = function() {
     $('.open-country-chooser').blur();
-    $('#grayout').show()
+    $('body').append('<div id="modal-grayout"></div>');
+    $('#modal-grayout').click(Gratipay.countryChooser.close);
     $('#country-chooser').show();
 };
 
 Gratipay.countryChooser.close = function() {
-    $('#country-chooser').hide()
-    $('#grayout').hide()
+    $('#country-chooser').hide();
+    $('#modal-grayout').remove();
 };
