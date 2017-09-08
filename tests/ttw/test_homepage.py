ee3ffc383e604d7863625328d78ad0c654f5eb18
Chad Whitacre <chad@zetaweb.com>
2017-09-07 20:24:23 -0400

    Redirect auth to /browse/projects(!)

--- tests/ttw/test_homepage.py
+++ tests/ttw/test_homepage.py
@@ -9,9 +9,9 @@ class Tests(BrowserHarness):
         assert self.css('#banner h1').html == 'Pay for open source.'
         assert self.css('#header .sign-in button').html.strip()[:17] == 'Sign in / Sign up'
 
-    def test_loads_for_authed(self):
+    def test_redirects_for_authed_exclamation_point(self):
         self.make_participant('alice', claimed_time='now')
         self.sign_in('alice')
         self.reload()
-        assert self.css('#banner h1').html == 'Pay for open source.'
+        assert self.css('#banner h1').html == 'Browse'
         assert self.css('.you-are a').html.strip()[:6] == '~alice'
