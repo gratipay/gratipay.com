743b3662d908b5bf4c0e50d7e87f53eb4179c3d7
mattbk <bedrocks@gmail.com>
2017-09-18 16:17:55 -0500

    Fix quotes.

--- tests/py/test_project_review_process.py
+++ tests/py/test_project_review_process.py
@@ -60,9 +60,9 @@ class Tests(QueuedEmailHarness):
             '{"body": "https://gratipay.com/foo/\\nhttps://gratipay.com/bar/\\n'
             'https://gratipay.com/baz/\\n\\n(This application will remain open '
             'for at least a week.)\\n\\nExample [shield](http://shields.io)) '
-            'for GitHub repositories:\\n"'
+            'for GitHub repositories:\\n'
             '[![Gratipay](https://img.shields.io/gratipay/project/foo/.svg)](https://gratipay.com/foo/)'
-            '\n> `[![Gratipay](https://img.shields.io/gratipay/project/foo/.svg)](https://gratipay.com/foo/)`', "title": "foo and 2 other projects"}')
+            '\n> `[![Gratipay](https://img.shields.io/gratipay/project/foo/.svg)](https://gratipay.com/foo/)`", "title": "foo and 2 other projects"}')
         assert kwargs['auth'] == ('cheeseburger', 'di3tc0ke')
 
 
