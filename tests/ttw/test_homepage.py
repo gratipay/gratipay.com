dc29cea442ee511ccc3e7f3f062f678c2415201e
Chad Whitacre <chad@zetaweb.com>
2017-10-05 07:01:02 -0400

    Try and close Travis race condition

--- tests/ttw/test_homepage.py
+++ tests/ttw/test_homepage.py
@@ -44,7 +44,8 @@ class Tests(BrowserHarness):
     def test_redirects_for_authed_exclamation_point(self):
         self.make_participant('alice', claimed_time='now')
         self.sign_in('alice')
-        self.reload()
+        with self.page_reload_afterwards():
+            self.visit('/')
         assert self.css('#banner h1').html == 'Browse'
         assert self.css('.you-are a').html.strip()[:6] == '~alice'
 
